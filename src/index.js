af8d3abf147a830bace536877f52122095c8bf03
Jundong2511 <jund2511@gmail.com>
2020-11-11 20:50:18 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Numberbutton(props){
+    return(
+        <button class="number-buttons" onClick={props.onClick}>{props.value}
+        <button>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class Display extends React.Component{
+
+    render(){
+        return{
+            <div id="display-area">{props.display}</div>
+        }
+    }
+
+
+class NumberButtons extends React.Component{
+
+
+    render(){
+        return{
+            <NumberButton value=(1-9)/>
+        }
+    }
+
+
+class FunctionButtons extends React.Component{
+    render(){
+        return{
+            
+            <button  class="operators-buttons" id="add" onClick={props.onClick})>+</button>
+
+        }}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleButtonsClick(){
+
+  }
+  handleFunctionClick(){
+
+  }
+    render() {
+    return {
+        <div id="calculator-box">
+        <Display />
+        <div id="buttons-area">
+        <FunctionButtons onClic={()=>this.handleFunctionClick()}>
+        <NumberButtons onClick={()=>this.handleButtonsClick()}/>
+        </div>
+        </div>
+
+    };
+  }
+}
+ReactDOM.render(<Main />, document.getElementById("root"));
